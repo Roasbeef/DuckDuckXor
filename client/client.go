a40620468d8005bb8cf721e6a345a08178ad9f4d
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-03-02 21:01:35 -0800

    Remove key size as a flag
    * We will now use 256-bit keys across the board

--- client/client.go
+++ client/client.go
@@ -6,7 +6,6 @@ import (
 )
 
 var (
-	keySize           = flag.Int("key_size", 32, "Key size to be used for PRF's and AES")
 	documentDirectory = flag.String("doc_dir", ".", "Directory where documents to be indexed live")
 )
 

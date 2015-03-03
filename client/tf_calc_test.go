b81a13f1931b7b2a50d4f9005daf26493413fcaf
Daniel Imberman <danielryan2430@gmail.com>
2015-03-03 15:11:00 -0800

    avoided a calamity

--- client/tf_calc_test.go
+++ client/tf_calc_test.go
@@ -3,45 +3,49 @@ package main
 import
 //"fmt"
 
-"testing"
+(
+	"fmt"
+	"testing"
+)
 
 // creating the doc list will assume that CorpusReader
 // is already working correctly
-func BuildDocumentChannel() chan *document {
-	c := NewCorpusReader("./test_directory")
-	c.Start()
-	return c.DocOut
-}
 
 func TestFrequencyWorker(t *testing.T) {
-	//b := BuildDocumentChannel()
-	c := NewCorpusReader("./test_directory/testFile1.txt")
-	c.Start()
-	tf := NewTermFrequencyCalculator(2, c.DocOut)
+	fmt.Printf("abcd\n")
+	corpusreader := NewCorpusReader("./test_directory/testFile1.txt")
+	corpusreader.Start()
+	preprocess := NewDocPreprocessor(corpusreader.DocOut)
+	preprocess.Start()
+	tf := NewTermFrequencyCalculator(1, preprocess.TfOut)
 	tf.wg.Add(1)
-	err := tf.frequencyWorker()
-	if err != nil {
-		t.Error("worker returned error: ", err)
-	}
+	fmt.Printf("start TfWorker")
+	go tf.frequencyWorker()
+	<-preprocess.InvIndexOut
+	<-preprocess.DocEncryptOut
 	m1 := <-tf.TermFreq
-	//m2 := <-tf.TermFreq
 	if m1["yummy"] != 3 {
-		//TODO ask lalu about actors hnadling multiple files
+		// 	//TODO ask lalu about actors hnadling multiple files
 		t.Error("map1 did not return correct term frequencies, \n gave answer", m1["yummy"])
 	}
 }
 
 func TestLiteralMapReducer(t *testing.T) {
-	c := NewCorpusReader("./test_directory")
-	c.Start()
-	tf := NewTermFrequencyCalculator(2, c.DocOut)
-	tf.wg.Add(1)
-	err := tf.frequencyWorker()
-	if err != nil {
-		t.Error("something went wrong while mapping")
-	}
-	a := tf.literalMapReducer()
-	if a["yummy"] != 4 {
-		t.Error("map did not reduce to a sum of values. gave value:", a["yummy"])
-	}
+
+	corpusreader := NewCorpusReader("./test_directory/testFile1.txt")
+	corpusreader.Start()
+	preprocess := NewDocPreprocessor(corpusreader.DocOut)
+	preprocess.Start()
+
+	// c := NewCorpusReader("./test_directory")
+	// c.Start()
+	// p := NewDocPreprocessor(c.DocOut)
+	// p.Start()
+	// tf := NewTermFrequencyCalculator(2, p.TfOut)
+	// tf.wg.Add(1)
+	// tf.frequencyWorker()
+	// a := tf.literalMapReducer()
+	// if a["yummy"] != 4 {
+	// 	t.Error("map did not reduce to a sum of values. gave value:", a["yummy"])
+	// }
 }

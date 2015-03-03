da8e2f8a274e1558b57b237e655c126740dd68e9
Daniel Imberman <danielryan2430@gmail.com>
2015-03-02 16:19:06 -0800

    started testing process for preprocessor, will begin process of modifying TF and InvIndex to use it

---
+++ client/doc_preprocesser_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPartitionStreams(t *testing.T) {
+	c := NewCorpusReader("./test_directory")
+	if c == nil {
+		t.Error("corpus not instantiated")
+	}
+	c.Start()
+
+	preprocesser := NewDocPreprocessor(c.DocOut)
+	preprocesser.wg.Add(2)
+	go preprocesser.partitionStreams()
+	d := <-preprocesser.TfOut
+	testFile1 := []string{"i", "like", "banana", "sandwich", "cats", "are", "fun", "yummy", "yummy", "yummy"}
+	for i, token := range d {
+		if token != testFile1[i] {
+			t.Error("incorrect input: expected %s but got %s", testFile1[i], token)
+		}
+	}
+	b := <-preprocesser.InvIndexOut
+	x := <-preprocesser.DocEncryptOut
+	d = <-preprocesser.TfOut
+	testFile2 := []string{"i", "like", "banana", "sandwich", "cats", "are", "fun", "yummy", "yummy", "yummy", "the", "cat", "in", "the", "hat", "ate", "a", "yummy", "banana", "and", "didnt", "like", "it"}
+	for i, token := range d {
+		if token != testFile2[i] {
+			t.Error("incorrect input: expected %s but got %s", testFile2[i], token)
+		}
+	}
+
+	if b != nil && x != nil {
+		fmt.Printf("asdgasg\n")
+	}
+
+}

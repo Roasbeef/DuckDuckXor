6556a9681bb99fb01277d715ac70006ba3c9cdc5
Olaoluwa Osuntokun <laolu32@gmail.com>
2015-03-05 22:13:10 -0800

    Initial pass at tests for KeyManager

---
+++ client/key_manager_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"crypto/rand"
+	"fmt"
+	"testing"
+
+	"github.com/conformal/btcwallet/snacl"
+)
+
+func TestInitialKeyDerivation(t *testing.T) {
+}
+
+func TestRegularSetup(t *testing.T) {
+}
+
+func TestUpdateKeyMap(t *testing.T) {
+}
+
+func TestLoadDbKeys(t *testing.T) {
+}
+
+func TestStoreEncryptedKeys(t *testing.T) {
+}
+
+func TestInverseHashTreeKeyDerivation(t *testing.T) {
+	var parentKey [keySize]byte
+	if _, err := rand.Read(parentKey[:]); err != nil {
+		t.Fatalf("Unable to read random number: %v", err)
+	}
+
+	derivedChildren1, err := inverseHashTreeKeyDerivation(parentKey, 2)
+	if err != nil {
+		t.Fatalf("Unable to perform inverse hash tree derivation: %v", err)
+	}
+	derivedChildren2, err := inverseHashTreeKeyDerivation(parentKey, 2)
+	if err != nil {
+		t.Fatalf("Unable to perform inverse hash tree derivation: %v", err)
+	}
+
+	if !bytes.Equal(derivedChildren1[0][:], derivedChildren2[0][:]) {
+		t.Fatalf("Derivation is not deterministic %v vs %v",
+			derivedChildren1[0], derivedChildren2[0])
+	}
+
+	fmt.Println(derivedChildren1, derivedChildren2)
+}
+
+func TestDeriveChildren(t *testing.T) {
+	var parentKey [keySize]byte
+	if _, err := rand.Read(parentKey[:]); err != nil {
+		t.Fatalf("Unable to read random number: %v", err)
+	}
+
+	children := deriveChildren(parentKey)
+	fmt.Println(children)
+}
+
+func TestEncryptChildKeys(t *testing.T) {
+	var mKey snacl.CryptoKey
+	if _, err := rand.Read(mKey[:]); err != nil {
+		t.Fatalf("Unable to read random number: %v", err)
+	}
+
+	// TODO(roasbeef): Have marshalled key in file?
+	masterKey := snacl.SecretKey{Key: &mKey}
+
+	// Read some random data for each child key.
+	childKeys := make([][keySize]byte, 6)
+	for _, k := range childKeys {
+		if _, err := rand.Read(k[:]); err != nil {
+			t.Fatalf("Unable to read random number: %v", err)
+		}
+	}
+
+	// Encrypt our child keys.
+	encryptedChildKeys, err := encryptChildKeys(&masterKey, childKeys)
+	if err != nil {
+		t.Fatalf("Unable to encrypt child keys: %v", err)
+	}
+
+	// Ensure that we can properly decrypt them.
+	for i, ec := range encryptedChildKeys {
+		regKey, err := masterKey.Decrypt(ec)
+		if err != nil {
+			t.Fatalf("Unable to decrypt child key: %v", err)
+		}
+
+		if !bytes.Equal(regKey[:], childKeys[i][:]) {
+			t.Fatalf("Child key was not properly decrypted got %v need %v",
+				regKey, childKeys[i])
+		}
+	}
+}
+
+func TestRequestHandler(t *testing.T) {
+}
